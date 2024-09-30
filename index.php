f3854a88597038fc61e34ee7b3515f211b464db7
marcoweb <marcocarvalho.web@gmail.com>
2024-09-30 12:05:49 +0000

    .

--- index.php
+++ index.php
@@ -6,4 +6,5 @@ use DatabaseGateway\Application;
 $app = new Application();
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
+header("Access-Control-Allow-Headers: Content-Type");
 echo json_encode($app->run());
